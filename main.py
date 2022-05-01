e6b6316f093078ae6ecaca6c56feb0bcc6920bc0
Nishchaya Jandyal <nishchayajandyal101@gmail.com>
2022-05-01 15:14:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import os
 
 from main import Dectector
 def main():
-    # videoPath="C:/Users/nishc/Downloads/City_Street.mp4"    # Video file
+    # videoPath="C:/Python/OpenCV/Projects/OD/City_Street.mp4"    # Video file
     videoPath=0     # Camera
     configPath=os.path.join("model_data","C:/Python/OpenCV/Projects/OD/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt")   # Change file path
     modelPath=os.path.join("model_data","C:/Python/OpenCV/Projects/OD/frozen_inference_graph (1).pb")   # Change file path
